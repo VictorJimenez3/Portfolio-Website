10685dc01f568b19a0993228735801f1539953a4
VictorJimenez3 <154997712+VictorJimenez3@users.noreply.github.com>
2025-03-06 18:28:30 -0500

    Initial Commit with Website Template without information

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Define routes
+@app.route('/')
+def animation():
+    return render_template('animation.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/example')
+def example():
+    return render_template('example.html')
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
